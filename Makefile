a941c305173afb252535a3d73e465a80537f478f
Vladimir Ilin <mark09072010@gmail.com>
2018-10-23 22:46:42 +0300

    fix psr version

--- Makefile
+++ Makefile
@@ -2,10 +2,10 @@ install:
 	composer install
 
 lint:
-	composer run-script phpcs -- --standard=PSR2 bin src tests
+	composer run-script phpcs -- --standard=PSR12 bin src tests
 
 lint-fix:
-	composer run-script phpcbf -- --standard=PSR2 src bin
+	composer run-script phpcbf -- --standard=PSR12 src bin
 
 test:
 	composer run-script phpunit tests
--- tests/DiffTest.php
+++ tests/DiffTest.php
@@ -10,13 +10,13 @@ class DiffTest extends TestCase
     public function testJson()
     {
         $expected = file_get_contents(__DIR__ . "/fixtures/expected.txt");
-        $actual = genDiff(__DIR__ ."/fixtures/before.json", __DIR__ ."/fixtures/after.json");
+        $actual = genDiff(__DIR__ . "/fixtures/before.json", __DIR__ . "/fixtures/after.json");
         $this->assertEquals($expected, $actual);
     }
     public function testYaml()
     {
         $expected = file_get_contents(__DIR__ . "/fixtures/expected.txt");
-        $actual = genDiff(__DIR__ ."/fixtures/before.yaml", __DIR__ ."/fixtures/after.yaml");
+        $actual = genDiff(__DIR__ . "/fixtures/before.yaml", __DIR__ . "/fixtures/after.yaml");
         $this->assertEquals($expected, $actual);
     }
 }
