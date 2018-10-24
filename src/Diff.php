ec61a0390daaa683b6186614ea8be3cff1146330
Vladimir Ilin <mark09072010@gmail.com>
2018-10-24 12:18:33 +0300

    change Diff

--- src/Diff.php
+++ src/Diff.php
@@ -8,15 +8,19 @@ function boolToStr($bool)
 {
     return $bool ? 'true' : 'false';
 }
+function getExtension($path)
+{
+    return pathinfo($path, PATHINFO_EXTENSION);
+}
+function getData($path)
+{
+    return file_get_contents($path, true);
+}
 
 function genDiff($pathBefore, $pathAfter)
 {
-    $extBefore = pathinfo($pathBefore, PATHINFO_EXTENSION);
-    $extAfter = pathinfo($pathAfter, PATHINFO_EXTENSION);
-    $dataBefore = file_get_contents($pathBefore);
-    $dataAfter = file_get_contents($pathAfter);
-    $before = parse($extBefore, $dataBefore);
-    $after = parse($extAfter, $dataAfter);
+    $before = parse(getExtension($pathBefore), getData($pathBefore));
+    $after = parse(getExtension($pathAfter), getData($pathAfter));
     $keys = array_unique(array_merge(array_keys($before), array_keys($after)));
     $arrOfLines = array_reduce($keys, function ($acc, $item) use ($before, $after) {
         $valueBefore = $before[$item] ?? '';
@@ -25,16 +29,16 @@ function genDiff($pathBefore, $pathAfter)
         $afterValue = is_bool($valueAfter) ? boolToStr($valueAfter) : $valueAfter;
         if (array_key_exists($item, $before) && array_key_exists($item, $after)) {
             if ($beforeValue === $afterValue) {
-                $acc = array_merge($acc, array("  $item: $beforeValue"));
+                $acc = array_merge($acc, ["  $item: $beforeValue"]);
             } else {
-                $acc = array_merge($acc, array("- $item: $beforeValue"), array("+ $item: $afterValue"));
+                $acc = array_merge($acc, ["- $item: $beforeValue"], ["+ $item: $afterValue"]);
             }
         }
         if (array_key_exists($item, $before) && !array_key_exists($item, $after)) {
-            $acc = array_merge($acc, array("- $item: $beforeValue"));
+            $acc = array_merge($acc, ["- $item: $beforeValue"]);
         }
         if (!array_key_exists($item, $before) && array_key_exists($item, $after)) {
-            $acc = array_merge($acc, array("+ $item: $afterValue"));
+            $acc = array_merge($acc, ["+ $item: $afterValue"]);
         }
         return $acc;
     }, []);
