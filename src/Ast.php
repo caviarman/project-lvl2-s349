397852a232731cc8cc257f1b6e95fe80d6c8e474
Vladimir Ilin <mark09072010@gmail.com>
2018-10-28 23:07:29 +0300

    refactor AST

--- src/Ast.php
+++ src/Ast.php
@@ -6,7 +6,16 @@ function boolToStr($bool)
 {
     return $bool ? 'true' : 'false';
 }
-
+function makeNode($type, $key, $before, $after, $children = null)
+{
+    return [
+        'type' => $type,
+        'key' => $key,
+        'beforeValue' => $before,
+        'afterValue' => $after,
+        'children' => $children
+    ];
+}
 function getAst($before, $after)
 {
     $keys = array_unique(array_merge(array_keys($before), array_keys($after)));
@@ -15,51 +24,20 @@ function getAst($before, $after)
         $valueAfter = $after[$item] ?? '';
         $beforeValue = is_bool($valueBefore) ? boolToStr($valueBefore) : $valueBefore;
         $afterValue = is_bool($valueAfter) ? boolToStr($valueAfter) : $valueAfter;
-        
         if (array_key_exists($item, $before) && array_key_exists($item, $after)) {
             if (is_array($beforeValue) && is_array($afterValue)) {
-                $properties = [
-                    'type' => 'nested',
-                    'key' => $item,
-                    'beforeValue' => null,
-                    'afterValue' => null,
-                    'children' => getAst($beforeValue, $afterValue)
-                ];
+                $properties = makeNode('nested',$item, null, null, getAst($beforeValue, $afterValue));
             } elseif ($beforeValue === $afterValue) {
-                $properties = [
-                    'type' => 'unchanged',
-                    'key' => $item,
-                    'beforeValue' => $beforeValue,
-                    'afterValue' => $afterValue,
-                    'children' => null
-                ];
+                $properties = makeNode('unchanged', $item, $beforeValue, $afterValue);
             } else {
-                $properties = [
-                    'type' => 'changed',
-                    'key' => $item,
-                    'beforeValue' => $beforeValue,
-                    'afterValue' => $afterValue,
-                    'children' => null
-                ];
+                $properties = makeNode('changed', $item, $beforeValue, $afterValue);
             }
         }
         if (array_key_exists($item, $before) && !array_key_exists($item, $after)) {
-            $properties = [
-                'type' => 'deleted',
-                'key' => $item,
-                'beforeValue' => $beforeValue,
-                'afterValue' => null,
-                'children' => null
-            ];
+            $properties = makeNode('deleted', $item, $beforeValue, null);
         }
         if (!array_key_exists($item, $before) && array_key_exists($item, $after)) {
-            $properties = [
-                'type' => 'added',
-                'key' => $item,
-                'beforeValue' => null,
-                'afterValue' => $afterValue,
-                'children' => null
-            ];
+            $properties = makeNode('added', $item, null, $afterValue);
         }
         return $properties;
     }, $keys);
